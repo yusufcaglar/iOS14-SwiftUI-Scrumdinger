44e31484fc14695503583ae3d236a28fed3b4ead
Yusuf Çağlar <faf7faf7@gmail.com>
2021-01-30 14:30:23 +0300

    Complete SwifUI and Xcode Essentials & Views in iOS App Dev with SwiftUI Tutorials from Apple.

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,49 @@
+//
+//  MeetingView.swift
+//  Scrumdinger
+//
+//  Created by Yusuf ÇAĞLAR on 30/01/2021.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Ellapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time Remaining")
+            .accessibilityLabel("10 minutes")
+            Circle()
+                .strokeBorder(lineWidth: 24, antialiased: true)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                   Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next Speaker")
+            }
+        }
+        .padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
