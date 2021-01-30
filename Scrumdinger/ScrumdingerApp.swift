a6e747aa89834ad92583c4e0c08a030cb5b52a6e
Yusuf Çağlar <faf7faf7@gmail.com>
2021-01-30 09:24:43 +0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Yusuf ÇAĞLAR on 30/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
