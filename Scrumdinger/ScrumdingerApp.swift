c242aa6bb746123a2fb92edef4e47eab113e0f73
Yusuf Çağlar <faf7faf7@gmail.com>
2021-02-02 18:29:03 +0300

    Complete Persistence, in iOS App Dev with SwiftUI Tutorials from Apple.

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -9,11 +9,16 @@ import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
-    @State private var scrums = DailyScrum.data
+    @ObservedObject private var data = ScrumData()
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                ScrumsView(scrums: $scrums)
+                ScrumsView(scrums: $data.scrums) {
+                    data.save()
+                }
+            }
+            .onAppear {
+                data.load()
             }
         }
     }
